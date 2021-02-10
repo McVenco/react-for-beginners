b8b594833d4e476a4b41aedf1dbbfb02e5171a34
Eric Vonk <mcvenco@gmail.com>
2021-02-10 22:27:45 +0100

    Video 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     fishes: {},
     order: {},
   };
+
   addFish = fish => {
     // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
@@ -19,18 +20,35 @@ class App extends React.Component {
     this.setState({
       fishes: fishes
     });
-    // this.setState({fishes});
   }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   }
+
+  addToOrder = (key) => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order })
+  }
+
   render () {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} /> )}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              /> 
+            ))}
           </ul>
         </div>
         <Order />
